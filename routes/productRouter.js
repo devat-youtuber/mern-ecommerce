29f1540e0c29dcba386dbc39af109430069fd14d
devat-youtuber <battuosai01@gmail.com>
2020-10-08 15:00:24 -0700

    first commit

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,15 +1,17 @@
 const router = require('express').Router()
 const productCtrl = require('../controllers/productCtrl')
+const auth = require('../middleware/auth')
+const authAdmin = require('../middleware/authAdmin')
 
 
 router.route('/products')
     .get(productCtrl.getProducts)
-    .post(productCtrl.createProduct)
+    .post(auth, authAdmin, productCtrl.createProduct)
 
 
 router.route('/products/:id')
-    .delete(productCtrl.deleteProduct)
-    .put(productCtrl.updateProduct)
+    .delete(auth, authAdmin, productCtrl.deleteProduct)
+    .put(auth, authAdmin, productCtrl.updateProduct)
 
 
 
